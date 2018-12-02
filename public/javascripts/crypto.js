0f222c707c62e474f01514a3d2ecd27a3a94e788
Mia Filic <mia.filic@fri.uni-lj.si>
2018-12-02 21:39:43 +0000

    trial

---
+++ public/javascripts/crypto.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+
+    // Close modals with ESC
+    window.addEventListener("keydown", function (e) {
+        if(e.which == 27) $(".modal").each(function() {
+            $(this).css("display", "none");
+        });
+    });
+});
